180bbac6aef3ebf8fa2791a2924dbde69cd49332
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-02-07 12:05:43 -0700

    all but one controller function working

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const PORT = 4040
 
 app.use(express.json())
 
-//app.get('/api/family', familyCtrl.getFamily)
-//app.post('/api/family', familyCtrl.addMember)
-//app.put('/api/family/:id', familyCtrl.editName)
-//app.delete('/api/family/:id', familyCtrl.buryMember)
+app.get('/api/family', familyCtrl.getFamily)
+app.post('/api/family', familyCtrl.addMember)
+app.put('/api/family/:id', familyCtrl.editName)
+app.delete('/api/family/:id', familyCtrl.buryMember)
 
 app.listen(PORT, ()=> console.log(`listening on port ${PORT}`))
