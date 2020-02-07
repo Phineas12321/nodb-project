8ae9891c65ed40c6245a7b753fce5486ade84399
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-02-07 11:11:59 -0700

    family array exists

--- server/controllers/familyController.js
+++ server/controllers/familyController.js
@@ -1,9 +1,27 @@
 const express = require('express')
 
-const family = []
+const family = [{name: 'You', id: 0}]
 
-let id = 0
+let id = 1
+
+
 
 module.exports = {
-    
+    getFamily: (req, res) => {
+        res.status(200).send(family)
+    },
+
+    addMember: (req, res) => {
+
+    },
+
+    editName: (req, res) => {
+
+    },
+
+    buryMember: (req, res) => {
+        let index = family.findIndex(() => {})
+        family.splice(index, 1)
+        res.status(200).send(family)
+    }
 }
--- server/index.js
+++ server/index.js
@@ -10,6 +10,6 @@ app.use(express.json())
 //app.get('/api/family', familyCtrl.getFamily)
 //app.post('/api/family', familyCtrl.addMember)
 //app.put('/api/family/:id', familyCtrl.editName)
-//app.delete('/api/family/:id', familyCtrl.deleteMember)
+//app.delete('/api/family/:id', familyCtrl.buryMember)
 
 app.listen(PORT, ()=> console.log(`listening on port ${PORT}`))
