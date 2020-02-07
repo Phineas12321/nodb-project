a22c38e12eccc204c5439a46efbf41b785c39647
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-02-07 11:38:31 -0700

    all controller functions written

--- server/controllers/familyController.js
+++ server/controllers/familyController.js
@@ -1,4 +1,3 @@
-const express = require('express')
 
 const family = [{name: 'You', id: 0}]
 
@@ -12,14 +11,28 @@ module.exports = {
     },
 
     addMember: (req, res) => {
+        const {member} = req.body
+        member.id = id
+        id++
 
+        family.push(member)
+
+        res.status(200).send(family)
     },
 
     editName: (req, res) => {
+        const {id} = req.params
+        const {name} = req.body
 
+        let index = family.findIndex((e) => e.id === +id)
+
+        family[index].name = name
+
+        res.status(200).send(family)
     },
 
     buryMember: (req, res) => {
+        const {id} = req.params
         let index = family.findIndex((e) => e.id === +id)
         family.splice(index, 1)
         res.status(200).send(family)
