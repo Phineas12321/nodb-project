596bbe7341c394da67983123e8731330ff7c854e
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-02-07 10:49:46 -0700

    added controllers kinda

---
+++ server/controllers/familyController.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const family = []
+
+let id = 0
+
+module.exports = {
+    
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const familyCtrl = require('./controllers/familyController')
 
 const app = express()
 
 const PORT = 4040
 
 app.use(express.json())
+
+//app.get('/api/family', familyCtrl.getFamily)
+//app.post('/api/family', familyCtrl.addMember)
+//app.put('/api/family/:id', familyCtrl.editName)
+//app.delete('/api/family/:id', familyCtrl.deleteMember)
 
 app.listen(PORT, ()=> console.log(`listening on port ${PORT}`))
